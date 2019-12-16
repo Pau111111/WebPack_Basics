5998ad1f9ee841fa41d22d89c483ec3efc18664b
Pau111111 <paugarciapellicer96@gmail.com>
2019-12-16 10:41:42 +0100

    project base

---
+++ assets/js/module-a.js
@@ -0,0 +1,19 @@
+//MODULO DE JAVASCRIPT QUE HAGA USO DE LAS FUNCIONALIDADES DE ECMASript 6
+
+//ARROW FUNCTION
+
+//Nueva estructura de clases ECMAScript 6
+
+//Template Strings
+
+//Let y Const
+
+export default class Cat(){
+    constructor(name, age){
+        this.name = name;
+        this.age = age;
+    }
+    sayHello(){
+        console.log("Hi " + this.name);
+    }
+};
